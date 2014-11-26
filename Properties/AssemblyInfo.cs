1c1599e9001335ae55a65eef025b44599ba8d2c2
Michael Yu <ItzWarty@gmail.com>
2014-11-25 20:31:28 -0800

    Update AssemblyInfo.cs in preparation for NuGet package release.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("libdnode")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dargon Nodes library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Dargon Project")]
 [assembly: AssemblyProduct("libdnode")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © ItzWarty 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
